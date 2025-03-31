967ae9b30815314fc95b8b67d0c82a4b6d954e55
Pedro Manse <pedro.manse@dmk3.com.br>
2025-03-30 21:06:21 -0300

    I've no ideia on how to do this in haskell :(

--- haskell/todo.hs
+++ haskell/todo.hs
@@ -1,41 +1,58 @@
+import Control.Exception (IOException, catch)
+import Data.ByteString (hGetContents, unpack)
 import Data.List
+import GHC.IO.IOMode (IOMode (ReadMode, WriteMode))
 import System.Environment
+import GHC.IO.StdHandles (openFile)
+import Data.Text.Encoding (decodeUtf8)
 
 main =
   let
     args = getArgs
    in
     do
-      putStrLn "Hello"
+      todoFile <- openFile ".todo" ReadMode
+      tds <- hGetContents todoFile
+      args <- args
+      delegate args $ read $ show tds
+
+safeLoadFile :: FilePath -> IO [String]
+safeLoadFile p = lines <$> readFile p `catch` ((\_ -> return []) :: (IOException -> IO String))
+
+delegate :: [String] -> [Todo] -> IO ()
+delegate [] tds = putStrLn $ showTodos tds
+delegate ["ls"] tds = putStrLn $ showTodos tds
+delegate ["add", td] tds = saveTodos $ addTodo td tds
+delegate ["rm", td] tds = saveTodos $ onTodo td tds deleteTodo
+delegate ["flip", td] tds = saveTodos $ onTodo td tds flipTodo
+delegate x _ = putStrLn $ "Command " ++ head x ++ " Doesn't exist"
+
+saveTodos = writeFile ".todo" . showTodos
+
+showTodos :: [Todo] -> String
+showTodos = intercalate "\n" . map show
 
 data Todo = Todo
   { text :: String
   , done :: Bool
   }
 
-emap :: (a -> Maybe b) -> (Maybe a -> Maybe b)
-emap f (Just a) = f a
-emap _ Nothing = Nothing
+addTodo :: String -> [Todo] -> [Todo]
+addTodo text list = Todo{text, done = False} : list
 
-onTodo' :: String -> [Todo] -> (Maybe Todo -> Maybe Todo) -> [Todo]
-onTodo' find [] exec = case exec Nothing of
-  Just a -> [a]
-  Nothing -> error ""
-onTodo' find (Todo{text, done} : xs) exec
-  | find == text = case exec $ Just (Todo{text, done = not done}) of
+onTodo :: String -> [Todo] -> (Todo -> Maybe Todo) -> [Todo]
+onTodo find [] _ = error $ "No such TODO with text: " ++ find
+onTodo find (Todo{text, done} : xs) exec
+  | find == text = case exec $ Todo{text, done = not done} of
       Just a -> a : xs
       Nothing -> xs
-  | otherwise = Todo{text, done} : onTodo' find xs exec
+  | otherwise = Todo{text, done} : onTodo find xs exec
 
 flipTodo :: Todo -> Maybe Todo
 flipTodo Todo{text, done} = Just Todo{text, done = not done}
 
 deleteTodo :: Todo -> Maybe Todo
 deleteTodo _ = Nothing
-
-addTodo :: String -> Maybe Todo -> Maybe Todo
-addTodo tx Nothing = Just Todo{text = tx, done = False}
-addTodo _ x = x
 
 doneBox :: Bool -> [Char]
 doneBox False = "[ ]"
